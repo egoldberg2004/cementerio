84443fe90de72d7e20cd09432e33443b5ee4eb74
egoldberg2004 <goldberg2004@gmail.com>
2017-03-14 10:29:57 -0300

    Conexion a Base de Datos

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<? php
+<?php
+  include('controlador/abre_bd.php');
+?>
 
-echo "esta es mi primera pagina";
+	<html>
+	<head>
+	<title>Sistema de Control y Gestion Cementerio</title>
+	</head>
 
- ?>
+				<frameset rows="160,*" cols="*" frameborder="no" border="0" framespacing="0">
+					<frame src="./arriba.asp" name="topFrame" scrolling="no" noresize >
+					<frameset cols="220,*" frameborder="no" border="0" framespacing="0">
+						<frame src="./menu.asp" name="leftFrame" scrolling="yes" noresize>
+						<frame src="./inc/portada.asp" name="mainFrame" scrolling="yes">
+					</frameset>
+				</frameset>
+
+	</html>
+
+  <?php
+    include('controlador/cierra_bd.php');
+  ?>
